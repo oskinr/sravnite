d311c525aa31af9ac36207216c1be3ad1e313bab
oskinr <30344806+oskinr@users.noreply.github.com>
2025-03-18 10:10:27 +0300

    Update переменная.py
    добавил возврат значения getoм

--- переменная.py
+++ переменная.py
@@ -4,12 +4,12 @@ from tkinter.ttk import Combobox
 
 
 def show_values1():
-    global a
+    global rows
 
-    a = combo.get()
-    if a == '':
-      a = "0"
-    print(a)
+    rows = combo.get()
+    if rows == '':
+      rows = "0"
+    print(rows)
 
 
 
@@ -23,8 +23,11 @@ root.geometry("220x60")
 bu1=Button(text='Enter', command = show_values1)
 bu1.place(x = 5, y = 1)
 
-#комбобоксы для ввода ключа слияния и сравнения
-combo = Combobox (root, values='номер')
+#комбобоксы для ввода
+combo = Combobox (root, values=[0,6])
+#Можно без условия и функций сразу из комбобокса возвращать get ом значение 0 или 6
+# combo.current(0)#номер значения по порядку
+# rows = combo.get() #Возвращает значение в rows по номеру ноль по порядку current из комбобокса
 combo.place(x = 50, y = 1)
 
 
